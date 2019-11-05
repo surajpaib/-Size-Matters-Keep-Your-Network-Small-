ee18fd87073dc821b4b1339ab177ec02658c1a81
Suraj Pai <surajballambat@gmail.com>
2019-11-05 17:34:05 +0100

    Linted Code

--- framework/BaseClass.py
+++ framework/BaseClass.py
@@ -1,26 +1,29 @@
 from includes import *
-from utils import *
+from utils import get_activation_scheme, input_validation
 
 class FullyConnectedNetwork(nn.Module):
+    """
+    """
     def __init__(self, params):
         super(FullyConnectedNetwork, self).__init__()
 
         # Validate Structure of Input
-        if not(input_validation(params)):
+        if not input_validation(params):
             exit()
 
         network_params = params['network']
         # Construct the Network Definition
-        self.input_layer = nn.Linear(network_params['input_layer']['units'], network_params['hidden_layer'][0]['units'])
+        self.input_layer = nn.Linear(network_params['input_layer']['units'], \
+            network_params['hidden_layer'][0]['units'])
         self.hidden_layers = []
         for i in range(1, len(network_params['hidden_layer'])):
-            self.hidden_layers.append(nn.Linear(network_params['hidden_layer'][i-1]['units'], network_params['hidden_layer'][i]['units']))
-        self.output_layer = nn.Linear(network_params['hidden_layer'][-1]['units'], network_params['output_layer']['units'])        
-        
-        
+            self.hidden_layers.append(nn.Linear(network_params['hidden_layer'][i-1]['units'],\
+                network_params['hidden_layer'][i]['units']))
+        self.output_layer = nn.Linear(network_params['hidden_layer'][-1]['units'],\
+                network_params['output_layer']['units'])
         # Get choice of activations or if missing default to values
-        self.activation_scheme = get_activation_scheme(network_params)     
-        print(self.activation_scheme)   
+        self.activation_scheme = get_activation_scheme(network_params)
+        print(self.activation_scheme)
 
     def forward(self, x):
         print(self.activation_scheme)
@@ -30,37 +33,37 @@ class FullyConnectedNetwork(nn.Module):
         x = self.activation_scheme[-1](self.output_layer(x))
         return x
 
-
-
-
 if __name__ == "__main__":
-    # Provide Network Description Here. Scalable to any number of units and activations within the Pytorch Functional API
+    # Provide Network Description Here. 
+    # Scalable to any number of units and activations within the Pytorch Functional API
     nn = FullyConnectedNetwork({
         "network":{
-        'input_layer': {
-            "units": 784
-            },
-        'hidden_layer': [{
-            "units": 100, 
-            }, {
-                "units": 4, 
-                "activation": "relu"
-                }, {
+            'input_layer': {
+                "units": 784
+                },
+            'hidden_layer': [{
+                    "units": 100, 
+                }, 
+                {
+                    "units": 4, 
+                    "activation": "relu"
+                },
+                {
                     "units": 5, 
                     "activation": "relu"
-                    },
-                    {
+                },
+                {
                     "units": 5, 
                     "activation": "relu"
-                    },
-                    {
+                },
+                {
                     "units": 5, 
                     "activation": "relu"
-                    }],
-        'output_layer': {
-            "units": 20,
-            "activation": "softmax"
-            }
+                }],
+            'output_layer': {
+                "units": 20,
+                "activation": "softmax"
+                }
         }
     })
 
