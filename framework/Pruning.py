0acf535dada8015332e6635da7429d8597bea156
smurf9852 <32539649+smurf9852@users.noreply.github.com>
2020-01-15 17:06:29 +0100

    l1 growing & equal weight initialisation

--- framework/Pruning.py
+++ framework/Pruning.py
@@ -1,11 +1,7 @@
 from BaseClass import BaseClass
 import copy
 import torch
-import matplotlib.pyplot as plt
-import time
 import numpy as np
-from captum.attr import LayerConductance
-
 
 class Pruning(BaseClass):
     def __init__(self, percentage):
@@ -16,11 +12,6 @@ class Pruning(BaseClass):
         self.new_model = copy.deepcopy(model)
         print("Set Model for Pruning: \n")
         self.print_model_structure(self.prev_model)
-
-
-    def set_test_data(self, data):
-        self.test_data = data[0]
-        self.test_target = data[1]
 
 
     def set_optimizer(self, optim):
@@ -36,8 +27,8 @@ class Pruning(BaseClass):
 
     def print_model_structure(self, model):
         for (layer, param) in enumerate(model.parameters()):
-            print("Layer {} , Parameters: {}".format(layer, param.shape))    
-                    
+            print("Layer {} , Parameters: {}".format(layer, param.shape))
+
 
     def define_strategy(self):
         """
@@ -47,86 +38,34 @@ class Pruning(BaseClass):
         # Access model weights
         self.neurons_retained = []
 
-        for p in self.new_model.named_parameters():
-            prune_idx = self.strategy(p)
-            if len(prune_idx) > 0:
-                self.neurons_retained.append(prune_idx)
+        for p in self.new_model.parameters():
+                if len(p.data.size()) != 1:
+                    normed_weights = p.data.abs()
+                    l1_norm_layer = [torch.sum(normed_weights[neuron_idx, :]).item() for neuron_idx in range(normed_weights.shape[0])]
+                    threshold = np.percentile(np.array(l1_norm_layer), self.pruning_perc * 100)
+                    prune_idx = np.argwhere(np.array(l1_norm_layer) > threshold).flatten()
+                    self.neurons_retained.append(prune_idx)
 
 
-    def l1_norm_pruning(self, p):
-        p = p[1]
-        print("Size:", len(p.data.size()))
-        if len(p.data.size()) != 1:
-            normed_weights = p.data.abs()
-            l1_norm_layer = [torch.sum(normed_weights[neuron_idx, :]).item() for neuron_idx in range(normed_weights.shape[0])]
-            threshold = np.percentile(np.array(l1_norm_layer), self.pruning_perc * 100)
-            prune_idx = np.argwhere(np.array(l1_norm_layer) > threshold).flatten()
-        else:
-            prune_idx = []
-
-        return prune_idx
-            
-
-    def layer_conductance_pruning(self, p):
-        _layer_name = p[0].split(".")
-        if len(_layer_name) == 3:
-            layer_name = _layer_name[0] + '[' + _layer_name[1] + ']'
-        elif len(_layer_name) == 2:
-            layer_name = _layer_name[0]
-
-        
-        if len(p[1].data.size()) != 1:
-        
-            cond = LayerConductance(self.new_model, eval('self.new_model.' + layer_name))
-            cond_vals = cond.attribute(self.test_data,target=self.test_target)
-            cond_vals = cond_vals.detach().numpy()
-            neuron_values = np.mean(cond_vals, axis=0)
-
-            visualize_importances(cond_vals.shape[1], neuron_values, p[0] + '{}'.format(time.time()))
-            threshold = np.percentile(np.array(neuron_values), self.pruning_perc * 100)
-            prune_idx = np.argwhere(np.array(neuron_values) > threshold).flatten()
-        else:
-            prune_idx = []
-
-        return prune_idx
-        
-  
     def apply_strategy(self):
         self.define_strategy()
-
-        # Create a copy of the neurons retained for each hidden layer weights, for example: [400, 50] and add another copy for the biases [400]
         self.neurons_retained = [val for val in self.neurons_retained for _ in (0, 1)]
 
-        # Convert parameters to list
         self.param_list = list(self.new_model.parameters())
 
-        # Iterate over all copy of neurons.
         for (i, neuron_idx) in enumerate(self.neurons_retained):
-            # Get all weights for a particular layer, including weights and biases.
             idx_weights = self.param_list[i]
-
-            # Condition check for all layers except the last 2. Last two layers do not prune number of neurons.
             if i < len(self.param_list) - 2:
                 print(neuron_idx)
-
-                # Set y as weights of all neurons to keep
                 y = idx_weights[neuron_idx]
-
-                # If the layer is not the first two layers (associated to input layers) and the layers are weights (weights have a shape of 2, biases have a shape of 1)
                 if i > 1 and len(idx_weights.shape)> 1:
-                    # Modify y in the second index to only keep the neurons in the previous layer.
                     y = y[:, self.neurons_retained[i-1]]
-
-            # If the layer is not the first 2 layers and is a weights layer then set the second index to neurons retained from previous layer.This condition only works for last 2 layers.
             elif i > 1 and len(idx_weights.shape) > 1:
                 y = idx_weights[:, self.neurons_retained[i-1]]
-                
-            # For all other layers set y as just all the weights. 
+
             else:
                 y = idx_weights
-                
 
-            # Set the weights data to y. editing the model parameters. 
             idx_weights.data = y
 
 
@@ -143,16 +82,7 @@ class Pruning(BaseClass):
     def get_optimizer_model(self):
         return self.get_optimizer(), self.get_model()
 
-    def prune_model(self, optimizer, model, strategy):
-        self.strategy = strategy
+    def prune_model(self, optimizer, model):
         self.set_optimizer_model(optimizer, model)
         self.apply_strategy()
         return self.get_optimizer_model()
-
-
-
-def visualize_importances(neuron_no, importances, name='1'):
-    x_pos = (np.arange(neuron_no))
-    plt.figure(figsize=(12,6))
-    plt.bar(x_pos, importances, align='center')
-    plt.savefig("{}.png".format(name))
