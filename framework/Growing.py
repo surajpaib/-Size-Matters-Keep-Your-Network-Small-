925297d08075fe2bb61f24774b9df91e0982e2a6
smurf9852 <32539649+smurf9852@users.noreply.github.com>
2020-01-15 19:24:13 +0100

    layer conductance growing

--- framework/Growing.py
+++ framework/Growing.py
@@ -3,6 +3,8 @@ import copy
 import torch
 import torch.nn as nn
 import numpy as np
+from captum.attr import LayerConductance
+
 
 class Growing(BaseClass):
     def __init__(self, percentage):
@@ -13,6 +15,10 @@ class Growing(BaseClass):
         self.new_model = copy.deepcopy(model)
         print("Set Model for Growing: \n")
         self.print_model_structure(self.prev_model)
+
+    def set_test_data(self, data):
+        self.test_data = data[0]
+        self.test_target = data[1]
 
     def set_optimizer(self, optim):
         self.prev_optimizer = optim
@@ -29,7 +35,7 @@ class Growing(BaseClass):
         for (layer, param) in enumerate(model.parameters()):
             print("Layer {} , Parameters: {}".format(layer, param.shape))
 
-    def define_strategy_random(self):
+    def random_growing(self):
 
         #Number of numbers to add per layer, list of integres
         self.number_neurons_per_layer = []
@@ -38,7 +44,7 @@ class Growing(BaseClass):
         for p in self.new_model.parameters():
             self.number_neurons_per_layer.append(np.random.randint(10))
 
-    def define_strategy(self):
+    def l1_norm_growing(self):
 
         #Number of numbers to add per layer, list of integres
         self.number_neurons_per_layer = []
@@ -68,6 +74,48 @@ class Growing(BaseClass):
         print(self.number_neurons_per_layer)
 
 
+    def layer_conductance_growing(self):
+
+        #Number of numbers to add per layer, list of integres
+        self.number_neurons_per_layer = []
+
+        meaned_cond_layer = []
+        neurons_per_layer = []
+
+        for p in [x for x in self.new_model.named_parameters()][:-2]: #skip output layer
+
+            if len(p[1].data.size()) != 1: #skip biases
+
+                _layer_name = p[0].split(".")
+                if len(_layer_name) == 3:
+                    layer_name = _layer_name[0] + '[' + _layer_name[1] + ']'
+                elif len(_layer_name) == 2:
+                    layer_name = _layer_name[0]
+
+                neurons_per_layer.append(p[1].data.shape[0])
+
+                cond = LayerConductance(self.new_model, eval('self.new_model.' + layer_name))
+                cond_vals = cond.attribute(self.test_data,target=self.test_target)
+                cond_vals = cond_vals.detach().numpy()
+                layer_value = np.mean(np.absolute(cond_vals))
+                meaned_cond_layer.append(layer_value)
+
+        #dont take into account output layer
+        total_number_neurons = np.sum(neurons_per_layer)
+        total_cond = np.sum(meaned_cond_layer)
+
+        print('Total number of neurons to divide: ',int(total_number_neurons*self.growing_perc))
+        print(meaned_cond_layer)
+
+        add_per_layer = [int(round((x/total_cond)*total_number_neurons*self.growing_perc,0)) for x in meaned_cond_layer]
+
+        self.number_neurons_per_layer = add_per_layer
+
+        print('Neurons to add per layer')
+        print(self.number_neurons_per_layer)
+
+    def define_strategy(self):
+        self.strategy()
 
     def apply_strategy(self):
         self.define_strategy()
@@ -106,9 +154,9 @@ class Growing(BaseClass):
             add_weights_2 = torch.zeros([current_num_nodes_2,number_new_neurons])
 
             #Randomize
-            nn.init.uniform_(add_weights_1, a = layer_weights_1.data.min().item() , b = layer_weights_1.data.max().item() )
+            nn.init.uniform_(add_weights_1, a = layer_weights_1.data.min().item() , b = layer_weights_1.data.max().item())
             nn.init.uniform_(add_bias_1,  a = layer_bias_1.data.min().item() , b = layer_bias_1.data.max().item())
-            nn.init.uniform_(add_weights_2, a = layer_weights_2.data.min().item() , b = layer_weights_2.data.max().item() )
+            nn.init.uniform_(add_weights_2, a = layer_weights_2.data.min().item() , b = layer_weights_2.data.max().item())
 
             #merge weights
             new_weights_1 = torch.cat([layer_weights_1,add_weights_1],dim=0) #add bottom row
@@ -134,7 +182,8 @@ class Growing(BaseClass):
     def get_optimizer_model(self):
         return self.get_optimizer(), self.get_model()
 
-    def grow_model(self, optimizer, model):
+    def grow_model(self, optimizer, model, strategy):
+        self.strategy = strategy
         self.set_optimizer_model(optimizer, model)
         self.apply_strategy()
         return self.get_optimizer_model()
