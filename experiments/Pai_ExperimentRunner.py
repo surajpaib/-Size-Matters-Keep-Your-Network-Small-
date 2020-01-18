59803ad4c3fb835100a45572f2d7efffd14fe36f
Suraj Pai <surajballambat@gmail.com>
2020-01-18 20:20:07 +0100

    Add more logs

--- experiments/Pai_ExperimentRunner.py
+++ experiments/Pai_ExperimentRunner.py
@@ -310,5 +310,5 @@ if __name__ == "__main__":
                                 'traini': experiment.traini,
                                 'params': experiment.params_dict
                             }, 'models/{}_{}_{}_{}.pth.tar'.format(uid, i_fold+1, epoch, _method))
-                    print('Distribution: ',distributions,' Percentage: ',str(perc_iter_tuple),' Fold ',str(i_fold),' epoch ',str(epoch))
+                    print('Distribution: ',distributions,' Percentage: ',str(perc_iter_tuple),' Fold ',str(i_fold),' epoch ',str(epoch), 'Acc: ', str(experiment.tacc), 'Network: ', str(experiment.network))
                     experiment.train_epoch(epoch)
