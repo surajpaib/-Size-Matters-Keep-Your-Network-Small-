ebb6d4beaa45fad9eaff4ab359d8f69c08397cad
Levi Kok <levilkok@gmail.com>
2020-01-19 12:35:28 +0100

    Levi file different (?) fix

--- experiments/Levi_ExperimentRunner.py
+++ experiments/Levi_ExperimentRunner.py
@@ -5,7 +5,7 @@ import shutil
 import string
 import random
 import os
-
+import copy
 import torch
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
@@ -19,7 +19,6 @@ from Experiment import Experiment
 from Pruning import Pruning
 from Growing import Growing
 from train_utils import ReshapeTransform
-
 
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
@@ -41,13 +40,13 @@ if __name__ == "__main__":
                 "learning_rate": 0.01,
                 "batch_size_test": 1000,
                 "n_epochs": 100,
-                "type": "Shifting",  # Pruning / Growing / Shifting
-                "method": "layer_conductance",  # layer_conductance / l1_norm
-                "method2": "l1_norm",  # none / layer_conductance / l1_norm
+                "type": "Shifting", # Pruning / Growing / Shifting
+                "method": "layer_conductance", # layer_conductance / l1_norm
+                "method2": "l1_norm", # none / layer_conductance / l1_norm
                 "percentage": perc_iter_tuple[0],
                 "percentage2": perc_iter_tuple[1],
                 "iterations": perc_iter_tuple[2],
-                "distribution": distributions  # "equal", "incr", "decr"
+                "distribution": distributions #"equal", "incr", "decr"
             }
             # batch_size_train = 100
             # learning_rate = 0.01
@@ -56,12 +55,12 @@ if __name__ == "__main__":
             # batch_size_test = 1000
             # n_epochs = 100
 
-            device = torch.device(
-                "cuda:0" if torch.cuda.is_available() else "cpu")
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
             if torch.cuda.is_available():
                 torch.cuda.manual_seed(seed)
             else:
                 torch.manual_seed(seed)
+
 
             experiment = Experiment(device)
 
@@ -73,79 +72,73 @@ if __name__ == "__main__":
                 growing = Growing(percentage=params_dict["percentage2"])
                 pruning = Pruning(percentage=params_dict["percentage"])
 
+
             model_dict = {
-                "network": {
+                "network":{
                     'input_layer': {
                         "units": 784,
 
-                    },
+                        },
                     'hidden_layer': [{
-                        "units": 168,
-                        "activation": "relu",
-                        "type": "Linear"
-                    },
+                            "units": 168,
+                            "activation": "relu",
+                            "type": "Linear"
+                        }
+                        ,
                         {
                             "units": 168,
                             "activation": "relu",
                             "type": "Linear"
 
-                    },
+                        },
                         {
                             "units": 168,
                             "activation": "relu",
                             "type": "Linear"
 
-                    }
-                    ],
+                        }
+                        ],
                     'output_layer': {
                         "units": 10,
                         "activation": "softmax",
                         "type": "Linear"
-                    }
+                        }
                 }
             }
 
             params_dict["model"] = model_dict
 
             train_dataset = torchvision.datasets.FashionMNIST('../data/', train=True, download=True,
-                                                              transform=torchvision.transforms.Compose([
-                                                                  torchvision.transforms.ToTensor(),
-                                                                  ReshapeTransform(
-                                                                      (-1,))
-                                                              ]))
+                transform=torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                ReshapeTransform((-1,))
+                ]))
 
-            test_dataset = torchvision.datasets.FashionMNIST('../data/', train=False, download=True,
-                                                             transform=torchvision.transforms.Compose([
-                                                                 torchvision.transforms.ToTensor(),
-                                                                 ReshapeTransform(
-                                                                     (-1,))
-                                                             ]))
+            test_dataset  = torchvision.datasets.FashionMNIST('../data/', train=False, download=True,
+                transform=torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                ReshapeTransform((-1,))
+                ]))
 
-            dataset = torch.utils.data.ConcatDataset(
-                [train_dataset, test_dataset])
+            dataset = torch.utils.data.ConcatDataset([train_dataset, test_dataset])
 
             kf = KFold(n_splits=5, shuffle=True, random_state=seed)
             for i_fold, (train_index, test_index) in enumerate(kf.split(dataset)):
                 # new fold - network from scratch
+                model = Network(model_dict)
                 params_dict["fold"] = i_fold+1
                 # set the dataloaders for the fold
                 train = torch.utils.data.Subset(dataset, train_index)
                 test = torch.utils.data.Subset(dataset, test_index)
-                train_loader = torch.utils.data.DataLoader(
-                    train, batch_size=params_dict["batch_size_train"], shuffle=True)
-                test_loader = torch.utils.data.DataLoader(
-                    test, batch_size=params_dict["batch_size_test"], shuffle=True)
+                train_loader = torch.utils.data.DataLoader(train, batch_size=params_dict["batch_size_train"], shuffle=True)
+                test_loader = torch.utils.data.DataLoader(test, batch_size=params_dict["batch_size_test"], shuffle=True)
 
                 # set up the experiment
                 experiment.set_metadata(params_dict)
                 experiment.set_network(model_dict)
                 experiment.set_loaders(train_loader, test_loader)
                 experiment.set_loss(torch.nn.CrossEntropyLoss())
-                experiment.set_optimizer(torch.optim.SGD(
-                    model.parameters(), lr=params_dict["learning_rate"]))
-                experiment.set_metadata(params_dict)
-                iter_list = experiment.get_iteration_distribution(
-                    params_dict["iterations"], params_dict["distribution"])
+                iter_list = experiment.get_iteration_distribution(params_dict["iterations"], params_dict["distribution"])
                 # training loop
                 for idx, epoch in enumerate(range(params_dict["n_epochs"])):
                     if iter_list[idx]:
@@ -164,36 +157,34 @@ if __name__ == "__main__":
                             _method = params_dict['method']+'_pruning'
 
                         eval(_type).set_test_data(next(iter(test_loader)))
-                        optimizer, model = eval(_type+'.'+_type_stam+'_model')(
-                            experiment.optimizer, experiment.network, eval(_type+'.'+_method))
+                        optimizer, model = eval(_type+'.'+_type_stam+'_model')(experiment.optimizer, experiment.network, eval(_type+'.'+_method))
                         experiment.set_optimizer(optimizer)
                         experiment.update_network(model)
 
                         if params_dict['type'] == "Shifting" and params_dict['method2'] != "none":
-                            # Growing after the pruning
+                            #Growing after the pruning
                             _method2 = params_dict['method2']+'_growing'
                             growing.set_test_data(next(iter(test_loader)))
-                            optimizer, model = growing.grow_model(
-                                experiment.optimizer, experiment.network, eval('growing.'+_method2))
+                            optimizer, model = growing.grow_model(experiment.optimizer, experiment.network, eval('growing.'+_method2))
                             experiment.set_optimizer(optimizer)
                             experiment.update_network(model)
 
                         experiment.save_weights({
-                            'epoch': epoch,
-                            'state_dict': experiment.network.state_dict(),
-                            'train_acc': experiment.tacc,
-                            'val_acc': experiment.acc,
-                            'train_loss': experiment.trainLoss,
-                            'val_loss': experiment.testLoss,
-                            'optimizer': experiment.optimizer.state_dict(),
-                            'traint': experiment.traint,
-                            'traini': experiment.traini,
-                            'params': experiment.params_dict
-                        }, 'models/{}_{}_{}_{}.pth.tar'.format(uid, i_fold+1, epoch, _method))
+                                'epoch': epoch,
+                                'state_dict': experiment.network.state_dict(),
+                                'train_acc': experiment.tacc,
+                                'val_acc': experiment.acc,
+                                'train_loss': experiment.trainLoss,
+                                'val_loss': experiment.testLoss,
+                                'optimizer' : experiment.optimizer.state_dict(),
+                                'traint': experiment.traint,
+                                'traini': experiment.traini,
+                                'params': experiment.params_dict
+                            }, 'models/{}_{}_{}_{}.pth.tar'.format(uid, i_fold+1, epoch, _method))
+
 
 
                     print('Distribution: ', distributions, ' Percentage: ', str(perc_iter_tuple), ' Fold ', str(i_fold), ' Epoch: ', str(epoch))
                     epoch_vals = experiment.train_epoch(epoch)
                     print(epoch_vals)
                     print(experiment.network)
-
